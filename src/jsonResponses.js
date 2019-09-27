fd96b0d5c085875a5af2a8555d5105fec1a1024e
Andy Ong <axo5586@rit.edu>
2019-09-26 22:36:59 -0400

    adding changes for debug

---
+++ src/jsonResponses.js
@@ -0,0 +1,139 @@
+
+const users = {};
+
+
+// function to send a json object
+const respondJSON = (request, response, status, object) => {
+  // set status code and content type (application/json)
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  // stringify the object (so it doesn't use references/pointers/etc)
+  // but is instead a flat string object.
+  // Then write it to the response.
+  response.write(JSON.stringify(object));
+  // Send the response to the client
+  response.end();
+};
+
+const respondJSONMeta = (request, response, status) => {
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.end();
+};
+
+// function for getting users
+const getUsers = (request, response) => {
+  // message to send
+  const responseJSON = {
+    users,
+    message: 'getUsers with GET',
+  };
+
+  // set our error message
+  responseJSON.message = 'getUsers with GET.';
+
+  // give the error a consistent id
+  responseJSON.id = 'badRequest';
+
+  // return our json with a 200 bad request code
+  return respondJSON(request, response, 200, responseJSON);
+};
+
+// function for getting users, head request
+const getUsersHead = (request, response) => {
+  // message to send
+  const responseJSON = {
+    message: 'getUsers with HEAD.',
+  };
+
+  // set our error message
+  responseJSON.message = 'getUsers with HEAD.';
+
+  // give the error a consistent id
+  responseJSON.id = 'badRequest';
+
+  // return our json with a 200 bad request code
+  return respondJSONMeta(request, response, 200);
+};
+
+// function for fake request
+const notReal = (request, response) => {
+  // message to send
+  const responseJSON = {
+    message: 'notReal with GET.',
+  };
+    // set our error message
+  responseJSON.message = 'notReal with GET.';
+
+  // give the error a consistent id
+  responseJSON.id = 'notReal';
+
+  // return our json with a 404 bad request code
+  return respondJSON(request, response, 404, responseJSON);
+};
+
+// function for fake request, head request
+const notRealHead = (request, response) => {
+  // message to send
+  const responseJSON = {
+    message: 'notReal with HEAD.',
+  };
+
+  // set our error message
+  responseJSON.message = 'notReal with HEAD.';
+
+  // give the error a consistent id
+  responseJSON.id = 'notRealHead';
+
+  // return our json with a 404 bad request code
+  return respondJSONMeta(request, response, 404, responseJSON);
+};
+
+// function to show not found error
+const notFound = (request, response) => {
+  // error message with a description and consistent error id
+  const responseJSON = {
+    message: 'The page you are looking for was not found.',
+    id: 'notFound',
+  };
+
+  // return our json with a 404 not found error code
+  respondJSON(request, response, 404, responseJSON);
+};
+
+const addUser = (request, response, body) => {
+  const responseJSON = {
+    message: 'Name and age are both required.',
+  };
+
+  if (!body.name || !body.age) {
+    responseJSON.id = 'missingParams';
+    return respondJSON(request, response, 400, responseJSON);
+  }
+
+  let responseCode = 201; // success created
+  if (users[body.name]) {
+    responseCode = 204;
+  } else {
+    users[body.name] = {};
+    users[body.name].name = body.name;
+  }
+
+  users[body.name].age = body.age;
+
+  if (responseCode === 201) {
+    responseJSON.message = 'Created Successfully';
+    return responseJSON(request, response, responseCode, responseJSON);
+  }
+
+  // catch
+  return respondJSONMeta(request, response, 204); // sending data to inform user body is updated
+};
+
+// exports to set functions to public.
+module.exports = {
+  getUsers,
+  getUsersHead,
+  notReal,
+  notRealHead,
+  notFound,
+  addUser,
+};
---
+++ src/server.js
@@ -0,0 +1,79 @@
+const http = require('http'); // pull in the http server module
+const url = require('url'); // pull in the url module
+// pull in the query string module
+const query = require('querystring');
+// pull in our html response handler file
+const htmlHandler = require('./htmlResponses.js');
+// pull in our json response handler file
+const jsonHandler = require('./jsonResponses.js');
+
+// set the port. process.env.PORT and NODE_PORT are for servers like heroku
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+// key:value object to look up URL routes to specific functions
+const urlStruct = {
+  '/': htmlHandler.getIndex,
+  '/getUsers': jsonHandler.success,
+  '/notReal': jsonHandler.badRequest,
+  notFound: jsonHandler.notFound,
+};
+
+const handlePost = (request, response, parsedUrl) => {
+  // handles errors
+  if (parsedUrl.pathname === '/addUser') {
+    const body = [];
+
+    request.on('error', () => {
+      response.statusCode = 400; // bad request status code
+      response.end();
+    });
+
+
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    });
+
+    request.on('end', () => {
+      const bodyString = Buffer.concat(body).toString(); // makes it a string
+      const bodyParams = query.parse(bodyString); // makes it a jsonObject
+      jsonHandler.addUser(request, response, bodyParams);
+    });
+  }
+};
+
+const handleGet = (request, response, parsedUrl) => {
+  if (parsedUrl.pathname === '/style.css') {
+    htmlHandler.getCSS(request, response);
+  } else if (parsedUrl.pathname === '/getUsers') {
+    jsonHandler.getUsers(request, response);
+  } else {
+    htmlHandler.getIndex(request, response);
+  }
+};
+
+// handle HTTP requests. In node the HTTP server will automatically
+// send this function request and pre-filled response objects.
+const onRequest = (request, response) => {
+  // parse the url using the url module
+  // This will let us grab any section of the URL by name
+  const parsedUrl = url.parse(request.url);
+
+  // check if the path name (the /name part of the url) matches
+  // any in our url object. If so call that function. If not, default to index.
+  if (urlStruct[parsedUrl.pathname]) {
+    urlStruct[parsedUrl.pathname](request, response);
+  } else {
+    urlStruct.notFound(request, response);
+  }
+
+  if (request.method === 'post') {
+    handlePost(request, response, parsedUrl);
+  } else {
+    handleGet(request, response, parsedUrl);
+  }
+};
+
+// start HTTP server
+http.createServer(onRequest).listen(port);
+
+console.log(`Listening on 127.0.0.1: ${port}`);
